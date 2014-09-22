5332f6fe6d1ca7454119a4a5184e5ac174ed3b5c
Jude Quintana <jude.quintana@gmail.com>
2014-09-22 10:11:44 -0600

    code clean up

--- lib/parse_response.rb
+++ lib/parse_response.rb
@@ -20,49 +20,34 @@ class HTTPResponseParser
   end
 
   def body
-
     @http_res =~ /(<!DOCTYPE html>.*<\/html>)/m
     $1.gsub!("\n","")
-
   end
 
   def header
-
     headers_hsh = {}
 
     @http_res =~ /(Server.*)\n\n/m
-
     headers = $1
 
     headers.split("\n").each do |h|
-
       h =~ /(^.*\:)\s(.*)/
       headers_hsh[$1] = $2
-
     end
 
     headers_hsh
   end
 
   def content_type
-
     header["Content-Type:"]
-
   end
 
   def server
-
     header["Server:"]
-
-
   end
 
   def location
-
     header["Location:"]
-
   end
-
-
 
 end
